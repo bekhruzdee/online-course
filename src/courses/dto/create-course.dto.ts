8d67a62e5129837826d1ce41505ec819860fb94c
bekhruzdee <qutbidinovbehruz003@gmail.com>
2026-03-02 10:02:16 +0500

    update courses

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,28 +1,3 @@
-// import {
-//   IsNotEmpty,
-//   IsString,
-//   IsNumber,
-//   Min,
-//   IsOptional,
-// } from 'class-validator';
-// import { Type } from 'class-transformer';
-
-// export class CreateCourseDto {
-//   @IsNotEmpty()
-//   @IsString()
-//   title: string;
-
-//   @IsNotEmpty()
-//   @IsString()
-//   description: string;
-
-//   @IsOptional()
-//   @Type(() => Number)
-//   @IsNumber()
-//   @Min(0)
-//   price?: number;
-// }
-
 import {
   IsNotEmpty,
   IsString,
--- src/courses/dto/search-course.dto.ts
+++ src/courses/dto/search-course.dto.ts
@@ -1,19 +1,3 @@
-// import { IsOptional, IsString, IsEnum, MaxLength } from 'class-validator';
-// import { CourseStatus } from '../entities/course.entity';
-// import { Transform } from 'class-transformer';
-
-// export class SearchCourseDto {
-//   @IsOptional()
-//   @IsString()
-//   @MaxLength(100)
-//   @Transform(({ value }) => value?.trim())
-//   title?: string;
-
-//   @IsOptional()
-//   @IsEnum(CourseStatus)
-//   status?: CourseStatus;
-// }
-
 import {
   IsOptional,
   IsString,
