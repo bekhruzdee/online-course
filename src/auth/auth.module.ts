248e1723202c496fdd85cf168be1f7c9f90df13b
bekhruzdee <qutbidinovbehruz003@gmail.com>
2026-03-05 09:58:15 +0500

    updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,33 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { JwtModule } from '@nestjs/jwt';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { AuthService } from './auth.service';
-// import { AuthController } from './auth.controller';
-// import { User } from 'src/users/entities/user.entity';
-// import { AuthGuard } from './auth.guard';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-//     TypeOrmModule.forFeature([User]),
-//     JwtModule.registerAsync({
-//       imports: [ConfigModule],
-//       inject: [ConfigService],
-//       useFactory: (config: ConfigService) => ({
-//         secret: config.get<string>('JWT_SECRET') || 'default_secret',
-//         signOptions: {
-//           expiresIn: (config.get<string>('JWT_EXPIRES_IN') as any) || '1h',
-//         },
-//       }),
-//     }),
-//   ],
-//   controllers: [AuthController],
-//   providers: [AuthService, AuthGuard],
-//   exports: [AuthGuard, JwtModule],
-// })
-// export class AuthModule {}
-
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
